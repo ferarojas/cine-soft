bd93f1be1112283107c9b1c164b7f0038b6bc560
ferarojas <frojas@uis.edu.co>
2021-06-03 11:27:16 -0500

    primer esquema JUnit creadas

---
+++ SalaTest.java
@@ -0,0 +1,42 @@
+
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * The test class SalaTest.
+ *
+ * @author  (your name)
+ * @version (a version number or a date)
+ */
+public class SalaTest
+{
+    /**
+     * Default constructor for test class SalaTest
+     */
+    public SalaTest()
+    {
+    }
+
+    /**
+     * Sets up the test fixture.
+     *
+     * Called before every test case method.
+     */
+    @Before
+    public void setUp()
+    {
+    }
+
+    /**
+     * Tears down the test fixture.
+     *
+     * Called after every test case method.
+     */
+    @After
+    public void tearDown()
+    {
+    }
+}
