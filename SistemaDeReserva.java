eae5bd2de3c6238c689790e1b9ff465ca05e85f8
ferarojas <frojas@uis.edu.co>
2021-06-04 16:51:09 -0500

    Inicia implementacion SistemaDeReserva

--- SistemaDeReserva.java
+++ SistemaDeReserva.java
@@ -7,11 +7,23 @@ import java.util.ArrayList;
  */
 public class SistemaDeReserva
 {
+    private static final int INDEX_PRUEBA = 0;
+    private static final int FILA_PRUEBA = 0;
     private ArrayList<Funcion> funciones;
 
     public SistemaDeReserva()
     {
         funciones = new ArrayList<>();
+    }
+
+    boolean reservaAsientos(Funcion funcion, int cantidad)
+    {
+        return false;
+    }
+
+    public boolean cancela(Cliente cliente)
+    {
+        return false;        
     }
 
     public ArrayList<Funcion> getFunciones(String titulo)
@@ -29,29 +41,38 @@ public class SistemaDeReserva
         return "";
     }
 
-    boolean reservaAsientos(Cliente cliente, Funcion funcion, int cantidad)
+    public String getDetallesReserva(Funcion funcion)
     {
-        return false;
+        String detalles = "";
+        ArrayList<Cliente> clientesFuncion = funcion.getClientes();
+        if( clientesFuncion.size() == 0 )
+            return "No hay clientes en esa funcion.";
+        Cliente cliente = clientesFuncion.get(INDEX_PRUEBA);           
+        Asiento[] asientos = funcion.getSala().getFilas().get(FILA_PRUEBA).asientosReservados();
+        if( asientos.length != 0 ){
+            detalles = "Su reserva: Fila "+ (FILA_PRUEBA+1) +" - Asientos: ";
+            for( Asiento asiento : asientos ){
+                if( asiento.getCliente().getNombre().equals( cliente.getNombre() ) )
+                    detalles += asiento.getNumero() +"  ";
+            }
+        }
+        else
+            detalles += "Esta funcion no tiene reservas.";
+        return detalles;
+    }
+
+    public void agregaFuncion(Funcion funcion){
+        funciones.add(funcion);
+    }
+
+    public ArrayList<Cliente> listaClientesFuncion(Funcion funcion)
+    {
+        return funcion.getClientes();
     }
 
     public ArrayList<Asiento> encuetraAsientosContiguos(Fila fila, int cantidad)
     {
         return null;
-    }
-
-    public String getDetallesReserva(Cliente cliente)
-    {
-        return null;
-    }
-
-    public ArrayList<Cliente> listaClientesFuncion(Funcion funcion)
-    {
-        return null;
-    }
-
-    public boolean cancela(Cliente cliente)
-    {
-        return false;        
     }
 
     public static void main(String[] args) {
